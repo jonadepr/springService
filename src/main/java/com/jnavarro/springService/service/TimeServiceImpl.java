a897f3102614b18f7782418450a23e8d887ebbb2
jonadepr <eljosele4@gmail.com>
2020-02-17 19:35:46 +0100

    arreglo edad

--- src/main/java/com/jnavarro/springService/service/TimeServiceImpl.java
+++ src/main/java/com/jnavarro/springService/service/TimeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.jnavarro.springservice.service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
@@ -11,7 +13,8 @@ import org.springframework.web.context.annotation.RequestScope;
  */
 
 @Service
-@RequestScope // Existirá uno por llamada (request), cambiamos el ámbito de aplicación, en general serán sin esto (Singleton)
+@RequestScope // Existirá uno por llamada (request), cambiamos el ámbito de aplicación, en
+              // general serán sin esto (Singleton)
 public class TimeServiceImpl implements TimeService {
 
     private Calendar c;
@@ -30,58 +33,11 @@ public class TimeServiceImpl implements TimeService {
 
     @Override
     public String edad(Integer aday, Integer amonth, Integer ayear) {
-        Date birthDate = new Date(ayear, amonth, aday);
-        //create calendar object for birth day
-        Calendar birthDay = Calendar.getInstance();
-        birthDay.setTimeInMillis(birthDate.getTime());
-
-        //create calendar object for current day
-        long currentTime = System.currentTimeMillis();
-        Calendar now = Calendar.getInstance();
-        now.setTimeInMillis(currentTime);
-
-        //Get difference between years
-        int years = now.get(Calendar.YEAR) - birthDay.get(Calendar.YEAR);
-        int currMonth = now.get(Calendar.MONTH) + 1;
-        int birthMonth = birthDay.get(Calendar.MONTH) + 1;
-
-        //Get difference between months
-        int months = currMonth - birthMonth;
-
-        //if month difference is in negative then reduce years by one 
-        //and calculate the number of months.
-        if (months < 0)
-        {
-        years--;
-        months = 12 - birthMonth + currMonth;
-        if (now.get(Calendar.DATE) < birthDay.get(Calendar.DATE))
-            months--;
-        } else if (months == 0 && now.get(Calendar.DATE) < birthDay.get(Calendar.DATE))
-        {
-        years--;
-        months = 11;
-        }
-        int days;
-        //Calculate the days
-        if (now.get(Calendar.DATE) > birthDay.get(Calendar.DATE))
-        days = now.get(Calendar.DATE) - birthDay.get(Calendar.DATE);
-        else if (now.get(Calendar.DATE) < birthDay.get(Calendar.DATE))
-        {
-        int today = now.get(Calendar.DAY_OF_MONTH);
-        now.add(Calendar.MONTH, -1);
-        days = now.getActualMaximum(Calendar.DAY_OF_MONTH) - birthDay.get(Calendar.DAY_OF_MONTH) + today;
-        } 
-        else
-        {
-        days = 0;
-        if (months == 12)
-        {
-            years++;
-            months = 0;
-        }
-        }
-        //Create new Age object 
-        return "Tienes, dias: "+days+" meses: "+months+" anyos: "+ years;
-            
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String fechaNacString = aday+"/"+amonth+"/"+ayear;
+        LocalDate fechaNac = LocalDate.parse(fechaNacString, fmt);
+        LocalDate ahora = LocalDate.now();
+        Period periodo = Period.between(fechaNac, ahora);
+        return "Tu edad es: "+periodo.getYears()+" anyos, "+periodo.getMonths()+" meses y "+periodo.getDays()+" dias"; 
     }
 }
